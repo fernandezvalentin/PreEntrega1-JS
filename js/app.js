2b346e5472fede9a019a70dfdba7eba716cda266
Kevin Decibe <kevindecibe@gmail.com>
2024-03-26 19:12:05 -0300

    review pola

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const shopContent = document.getElementById("shopContent");
-const verCarrito = document.getElementById("verCarrito");
-const modalContainer = document.getElementById("modal-container");
-const cantidadCarrito = document.getElementById("cantidadCarrito");
+const verCarrito = document.getElementById("verCarrito"); 
+const modalContainer = document.getElementById("modal-container"); 
+const cantidadCarrito = document.getElementById("cantidadCarrito"); // Me gusta, te recomendaria que elijas una convencion de nomnbres, separado con - o en camelCase en este caso tenes 3 con "-" asique cambiaria modal-container por modalContainer 
 
 // Inicialización del carrito como un array vacío
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []; // lo declararia como constante ya que no hay una reasignacion, reasignar no es lo mismo que ejecutar un metodo de un objeto, hacer carrito.push() no cambia la referencia del array
 
 const getProducts = async () => {
   const response = await fetch("data.json");
@@ -23,7 +23,7 @@ const getProducts = async () => {
 
     let comprar = document.createElement("button");
     comprar.innerText = "Comprar";
-    comprar.className = "Comprar";
+    comprar.className = "Comprar"; // la clase comprar no existe no hace falta
 
     content.append(comprar);
 
@@ -86,4 +86,4 @@ const saveLocal = () => {
 };
 
 // Get Item
-JSON.parse(localStorage.getItem("carrito"));
+JSON.parse(localStorage.getItem("carrito")); // no estas haciendo nada con esto, ya lo haces en la linea 7, aca simplemente estas buscando el carrito en el localstorage pero no lo estas guardando en ninguna variable, por lo tanto es inutil
